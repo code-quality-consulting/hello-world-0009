f479b3b1a1674c555f875403794ff0dfd7f997dd
Zach Overhulser <zoverhulser@gmail.com>
2018-04-24 18:07:03 -0500

    Greets user in English with Hello world!

--- test.js
+++ test.js
@@ -3,10 +3,12 @@ const assert = require("assert");
 // assert.equal
 // Tests shallow, coercive equality between the actual and expected parameters using the Abstract Equality Comparison ( == ).
 
-function makeGreeting() {}
+function makeGreeting() {
+    return "Hello world!";
+}
 
 assert.equal(makeGreeting(), "Hello world!");
-
+console.log("Your test has passed!");
 /*
     1) Make a failing test
     2) Make it pass
