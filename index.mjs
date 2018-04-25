289d3f26149dc7a4515e8118350a4fb214336d6f
Zach Overhulser <zoverhulser@gmail.com>
2018-04-24 23:01:11 -0500

    Defaults language to English.

--- index.mjs
+++ index.mjs
@@ -1,4 +1,4 @@
-export function makeGreeting(name = "world", language) {
+export function makeGreeting(name = "world", language = "English") {
     if (language === "Spanish") {
         name = "mundo";
         return "¡Hola " + name + "!";
